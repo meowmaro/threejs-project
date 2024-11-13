68fbd808b1cdbdce44b0b342495869477f457941
4l0sh <2155399@roc-dev.com>
2024-11-13 13:47:54 +0100

    profile

--- main.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
-
-function openNav() {
-    if (document.getElementById('sideNav').className === 'sideNav')
-      document.getElementById('sideNav').className = 'open';
-    else document.getElementById('sideNav').className = 'sideNav';
-  }
